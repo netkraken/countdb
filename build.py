a41a78a5dc7271a1a596c58a0699b9499adab29b
Arne Hilmann <arne@hilmann.de>
2015-06-05 12:57:03 +0200

    extract countdb to own project

---
+++ build.py
@@ -0,0 +1,23 @@
+from pybuilder.core import use_plugin, init
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.install_dependencies")
+use_plugin("python.flake8")
+use_plugin("python.frosted")
+use_plugin("python.coverage")
+use_plugin("python.distutils")
+
+
+name = "counterdb"
+default_task = "publish"
+version = "0.2"
+
+
+@init
+def set_properties(project):
+    project.build_depends_on("mock")
+    project.depends_on("docopt")
+
+    project.set_property("flake8_verbose_output", True)
+    project.set_property("flake8_break_build", True)
